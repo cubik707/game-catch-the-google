c0ee1eaa1c4ed49690af38929f22b8ee7d7cfd1d
Vladislava <stdemidovez@gmail.com>
2024-09-24 17:05:50 +0300

    beforeEach in test

--- game.test.js
+++ game.test.js
@@ -1,14 +1,18 @@
 import {Game} from "./game";
 
 describe("Game", () => {
+    let game;
+
+    beforeEach(()=>{
+        game = new Game()
+    })
+
     it("should return Pending status as initial", async () => {
-        const game = new Game();
         let status = await game.getStatus()
         expect(status).toBe("PENDING")
     })
 
     it("should return In-progress status after start", async () => {
-        const game = new Game();
         await game.start()
         let status = await game.getStatus()
         expect(status).toBe("IN-PROGRESS")
